2e79ca6d51ccd09d62ccb7af56c1a0f78150e7a3
POO <carlosserranosf@gmail.com>
2025-04-24 08:19:38 -0500

    Mensaje añadido

--- src/Empresa.java
+++ src/Empresa.java
@@ -81,6 +81,7 @@ public class Empresa {
         empleado1.mostrarInformacion();
         empleado2.mostrarInformacion();
         empleado3.mostrarInformacion();
+        System.out.println("\nRegistro finalizado.");
 
         teclado.close();
     }
